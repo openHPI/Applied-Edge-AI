9e1a2133a048c3d54e8da3f6b0faeb5dbd5de12c
Ranji Raj <64376506+ranjiGT@users.noreply.github.com>
2022-07-22 18:30:07 +0200

    Update grader.py

--- Practical_Exercise_Week_1/grader.py
+++ Practical_Exercise_Week_1/grader.py
@@ -85,10 +85,10 @@ if __name__ == "__main__":
     print(f"Your score is: {user_score} / 4.0")
     print("*"*85)
     print("Debug Info")
-    os.environ['score'] = str(user_score/4)
+    os.environ['NBSCORE'] = str(user_score/4)
     os.environ['NBPATH'] = nbfile
     
-    print(os.environ['score'])
+    print(os.environ['NBSCORE'])
     print(os.environ['NBPATH'])
     
     # Submit button code
